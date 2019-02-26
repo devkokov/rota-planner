b0e7fbbf4d207ebade0a536944ce7b5b60c08821
dimitar <dimitar@burnthebook.co.uk>
2019-02-26 17:24:45 +0000

    added the ability to allocate hours per day and week for a given worker

--- src/Plan/WeekPlan.php
+++ src/Plan/WeekPlan.php
@@ -2,7 +2,9 @@
 
 namespace DevKokov\RotaPlanner\Plan;
 
+use DevKokov\RotaPlanner\Day\DayInterface;
 use DevKokov\RotaPlanner\Week\WeekInterface;
+use DevKokov\RotaPlanner\Worker\WorkerInterface;
 
 /**
  * Class WeekPlan
@@ -14,6 +16,9 @@ class WeekPlan implements WeekPlanInterface
 {
     private $week;
     private $dayPlans = [];
+
+    private $allocatedHoursPerDay = [];
+    private $allocatedHoursPerWeek = [];
 
     public function __construct(WeekInterface $week)
     {
@@ -36,5 +41,21 @@ class WeekPlan implements WeekPlanInterface
     public function getDayPlans(): array
     {
         return $this->dayPlans;
+    }
+
+    public function getAllocatedHoursPerWeek(WorkerInterface $worker)
+    {
+        return $this->allocatedHoursPerWeek[spl_object_hash($worker)] ?? 0;
+    }
+
+    public function getAllocatedHoursPerDay(WorkerInterface $worker, DayInterface $day)
+    {
+        return $this->allocatedHoursPerDay[get_class($day)][spl_object_hash($worker)] ?? 0;
+    }
+
+    public function allocateHours(WorkerInterface $worker, DayInterface $day, float $hours)
+    {
+        $this->allocatedHoursPerDay[get_class($day)][spl_object_hash($worker)] += $hours;
+        $this->allocatedHoursPerWeek[spl_object_hash($worker)] += $hours;
     }
 }
--- tests/unit/Plan/WeekPlanTest.php
+++ tests/unit/Plan/WeekPlanTest.php
@@ -2,10 +2,12 @@
 
 namespace DevKokov\RotaPlanner\Tests\Unit\Plan;
 
+use DevKokov\RotaPlanner\Day\DayInterface;
 use DevKokov\RotaPlanner\Plan\DayPlanInterface;
 use DevKokov\RotaPlanner\Plan\WeekPlan;
 use DevKokov\RotaPlanner\Plan\WeekPlanInterface;
 use DevKokov\RotaPlanner\Week\WeekInterface;
+use DevKokov\RotaPlanner\Worker\WorkerInterface;
 use PHPUnit\Framework\TestCase;
 
 class WeekPlanTest extends TestCase
@@ -33,5 +35,25 @@ class WeekPlanTest extends TestCase
         $dayPlan = $this->getMockBuilder(DayPlanInterface::class)->getMock();
         $plan->addDayPlan($dayPlan);
         $this->assertEquals([$dayPlan], $plan->getDayPlans());
+    }
+
+    public function testAllocateHours()
+    {
+        $plan = new WeekPlan(
+            $this->getMockBuilder(WeekInterface::class)->getMock()
+        );
+        $day1 = $this->getMockBuilder(DayInterface::class)->setMockClassName('Monday')->getMock();
+        $day2 = $this->getMockBuilder(DayInterface::class)->setMockClassName('Tuesday')->getMock();
+        $worker1 = $this->getMockBuilder(WorkerInterface::class)->getMock();
+        $worker2 = $this->getMockBuilder(WorkerInterface::class)->getMock();
+
+        $plan->allocateHours($worker1, $day1, 3.5);
+        $plan->allocateHours($worker1, $day2, 4.5);
+        $plan->allocateHours($worker2, $day1, 6);
+
+        $this->assertEquals(8, $plan->getAllocatedHoursPerWeek($worker1));
+        $this->assertEquals(6, $plan->getAllocatedHoursPerWeek($worker2));
+        $this->assertEquals(3.5, $plan->getAllocatedHoursPerDay($worker1, $day1));
+        $this->assertEquals(4.5, $plan->getAllocatedHoursPerDay($worker1, $day2));
     }
 }
